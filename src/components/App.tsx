1196b6017401d69df5b6715ed0fe3ec32df2478a
Dominic Geargeoura <dom@Dominics-MacBook-Pro.local>
2022-05-17 17:37:34 +0100

    updates

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,36 +9,45 @@
  */
 
 import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar, View } from 'react-native';
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  View,
+} from 'react-native';
 
 // import Text from './base/Text/Text';
 import SectionHeader from './base/SectionHeader/SectionHeader';
 import { colors } from '../globals/constants';
 
-const App = () => {
-  const backgroundStyle = {
+const text = {
+  heading: 'Find your getaway',
+  description:
+    'Our spaces are designed for comfort - whether you are working, relaxing, or craving some spaces',
+};
+
+const App = () => (
+  <SafeAreaView style={styles.safeAreaView}>
+    <StatusBar barStyle="dark-content" />
+    <ScrollView
+      contentInsetAdjustmentBehavior="automatic"
+      style={styles.scrollView}>
+      <SectionHeader heading={text.heading} description={text.description} />
+      <View>{/* <Text styles={{ color: 'red' }}>Hello Metalab!</Text> */}</View>
+    </ScrollView>
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  safeAreaView: {
     backgroundColor: colors.sand,
     flex: 1,
+  },
+  scrollView: {
     padding: 24,
     paddingTop: 0,
-  };
-  const headingCopy = 'Find your getaway';
-  const descriptionCopy =
-    'Our spaces are designed for comfort - whether you are working, relaxing, or craving some spaces';
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle="dark-content" />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <SectionHeader heading={headingCopy} description={descriptionCopy} />
-        <View>
-          {/* <Text styles={{ color: 'red' }}>Hello Metalab!</Text> */}
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  },
+});
 
 export default App;
